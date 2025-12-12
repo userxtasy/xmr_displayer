5833ecd0b27cc47c11b3bb8d6bdd0bba4658636b
sysXtasy <164731568+userxtasy@users.noreply.github.com>
2025-12-12 10:35:38 +0100

    update
    update so it fits better waybar

--- crypto.sh
+++ crypto.sh
@@ -1,16 +1,11 @@
-  GNU nano 8.7                                                      .scripts/crypto.sh                                                               
- 1 #!/bin/bash
- 2
- 3 while true; do
- 4     btc=$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur,usd")
- 5     xmr=$(curl -s "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=eur,usd")
- 6     eur_btc=$(echo $btc | jq -r '.bitcoin.eur')
- 7     eur_xmr=$(echo $xmr | jq -r '.monero.eur')
- 8     usd_btc=$(echo $btc | jq -r '.bitcoin.usd')
- 9     usd_xmr=$(echo $xmr | jq -r '.monero.usd')
-10
-11     echo " : $eur_btc € [$usd_btc $]  : $eur_xmr € [$usd_xmr $]"
-12     sleep 60
-13 done
-14
-15
+btc=$(/usr/bin/curl -s -H "User-Agent: Mozilla/5.0" \
+    "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur,usd")
+xmr=$(/usr/bin/curl -s -H "User-Agent: Mozilla/5.0" \
+    "https://api.coingecko.com/api/v3/simple/price?ids=monero&vs_currencies=eur,usd")
+
+eur_btc=$(echo "$btc" | /usr/bin/jq -r '.bitcoin.eur')
+usd_btc=$(echo "$btc" | /usr/bin/jq -r '.bitcoin.usd')
+eur_xmr=$(echo "$xmr" | /usr/bin/jq -r '.monero.eur')
+usd_xmr=$(echo "$xmr" | /usr/bin/jq -r '.monero.usd')
+
+echo -n " ${eur_btc} € [${usd_btc} $]  ${eur_xmr} € [${usd_xmr} $]"
